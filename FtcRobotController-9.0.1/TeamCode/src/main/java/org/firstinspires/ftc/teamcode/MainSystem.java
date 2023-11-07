7f3dd4d68a72c2f98221fd100c03e25b996f9f61
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-11-07 12:06:47 -0600

    Test this code. Subsystem tryout.

--- FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -1,21 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
 import org.firstinspires.ftc.teamcode.Subsystems.Elevator;
-
 
 @TeleOp
 public class MainSystem extends LinearOpMode {
 
-    Chassis chassis = new Obj;
-    Elevator elevator = new Obj;
+    Chassis chassis;
+    Elevator elevator;
 
     @Override
-    public static void Main() {
+    public void runOpMode() {
+        chassis = new Chassis(hardwareMap); // Create an instance of Chassis
+        elevator = new Elevator(); // Create an instance of Elevator
+
         waitForStart();
-            chassis.chassisLoop();
+
+        while (opModeIsActive()) {
+            chassis.chassisLoop(gamepad1);
             elevator.elevatorLoop();
             telemetry.addData("Status", "Go");
+            telemetry.update();
+        }
     }
 }
+
--- FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Chassis.java
+++ FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Chassis.java
@@ -1,24 +1,25 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Chassis {
 
     private DcMotor left_Drive;
     private DcMotor right_Drive;
 
-    public void Chassis() {
+    public Chassis(HardwareMap hardwareMap) {
         left_Drive = hardwareMap.get(DcMotor.class, "left_Drive");
         right_Drive = hardwareMap.get(DcMotor.class, "right_Drive");
     }
 
-    public void chassisloop() {
+    public void chassisLoop(Gamepad gamepad1) {
         float x = -gamepad1.left_stick_y;
         float y = -gamepad1.right_stick_x;
-        right_Drive.setPower(y-x);
-        left_Drive.setPower(x+y);
-
+        right_Drive.setPower(y - x);
+        left_Drive.setPower(x + y);
     }
-
 }
+
+
