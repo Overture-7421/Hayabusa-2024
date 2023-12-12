cf33606a8d4eb4af513b7ded1385fed0bc7c2db2
Rigoberto-Soto <A01571662@tec.mx>
2023-12-11 20:06:26 -0600

    Auto Imports Modified

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous_One.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous_One.java
@@ -1,19 +1,12 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
-import static java.lang.Thread.sleep;
-
-import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.MainSystem;
 
 
 @Autonomous
 public class Autonomous_One extends LinearOpMode {
-    Chassis chassis;
-
 
     public void runOpMode(){
         waitForStart();
