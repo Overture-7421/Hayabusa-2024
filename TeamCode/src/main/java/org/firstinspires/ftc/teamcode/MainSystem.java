eefa3250f742ffc913de3096b7db41439497a625
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2024-01-13 15:45:01 -0600

    This code version was used for the DriveTests 13JAN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -68,9 +68,9 @@ public class MainSystem extends LinearOpMode {
             }
 
             // Elevator
-            if (operatorGamepad.right_bumper) {
+            if (operatorGamepad.triangle) {
                 elevator.ElevatorVoltage(1); // Voltage TBD
-            } else if (operatorGamepad.left_bumper) {
+            } else if (operatorGamepad.circle) {
                 elevator.ElevatorVoltage(-1); // Voltage TBD
             } else {
                 elevator.ElevatorVoltage(0);
