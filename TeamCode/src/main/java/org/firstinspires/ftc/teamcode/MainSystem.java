d9bb1f792709ee7402c1cd6e330125ae70b27962
Marco <A015726012@tec.mx>
2024-01-18 17:36:55 -0600

    Command Base ready to test and code review

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -34,27 +34,28 @@ public class MainSystem extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
-
         chassis     = new Chassis(hardwareMap);     // Create an instance of Chassis
         elevator    = new Elevator(hardwareMap);    // Create an instance of Elevator
         intake      = new Intake(hardwareMap, "intake");      // Create an instance of Intake
         band        = new Band(hardwareMap);        // Create an instance of Band
         claw        = new Claw(hardwareMap);        // Create an instance of Claw
         shooter     = new Shooter(hardwareMap);     // Create an instance of Shooter
-        driverOp = new GamepadEx(gamepad1);         // Create an instance of DriverGamepad
-        toolOp = new GamepadEx(gamepad2);           //Create an instance of Opa
+        driverOp    = new GamepadEx(gamepad1);         // Create an instance of DriverGamepad
+        toolOp      = new GamepadEx(gamepad2);           //Create an instance of Opa
 
          // Intake and Band in
         Button buttonA = driverOp.getGamepadButton(GamepadKeys.Button.A);
         buttonA.whileHeld(new MoveIntake(intake,1));
         buttonA.whileHeld(new MoveBand(band,-1));
+
         // Intake and Band out
         Button buttonB = driverOp.getGamepadButton(GamepadKeys.Button.B);
         buttonB.whileHeld(new MoveIntake(intake,-1));
         buttonB.whileHeld(new MoveBand(band,1));
-        // Intake Movement
+
+        // Chassis Movement
         chassis.setDefaultCommand(new MoveChassis(chassis,gamepad1));
+
         // Shooter
         Button rightBumper = toolOp.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER);
         rightBumper.whileHeld(new MoveShooter(shooter,1));
@@ -62,67 +63,7 @@ public class MainSystem extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
             CommandScheduler.getInstance().run();
-            // Chassis
-//            chassis.setSpeed(driverGamepad.left_stick_y, driverGamepad.right_stick_x);
-//                //SlowMode
-//                if (driverGamepad.left_bumper) {
-//                    chassis.setSpeed(-driverGamepad.left_stick_y * 0.3,
-//                            -driverGamepad.right_stick_x * 0.3);
-//                } else {
-//                    chassis.setSpeed(-driverGamepad.left_stick_y, -driverGamepad.right_stick_x);
-//                }
-
-            /*/ Intake
-            if (operatorGamepad.right_bumper) {
-                intake.IntakeVoltage(2); // Voltage Pendant to Adjust
-            } else if (operatorGamepad.left_bumper) {
-                intake.IntakeVoltage(-2); // Voltage Pendant to Adjust
-            } else {
-                intake.IntakeVoltage(0);
-            }
-            */
-
-            // Claw
-//            if (operatorGamepad.x) {
-//                claw.ClawVoltage(1); // Voltage Pendant to Adjust
-//
-//            } else if(operatorGamepad.y) {
-//                claw.ClawVoltage(0); // Voltage Pendant to Adjust
-//            }
-
-            /*/ Band
-            if (operatorGamepad.dpad_up) {
-                band.BandVoltage(1); // Voltage Pendant to Adjust
-            } else if (operatorGamepad.dpad_down) {
-                band.BandVoltage(-1); // Voltage Pendant to Adjust
-            } else{
-                band.BandVoltage(0); // Voltage Pendant to Adjust
-            }
-             */
-
-            // Elevator
-//            if (operatorGamepad.triangle) {
-//                elevator.ElevatorVoltageMotor1(1); // Voltage TBD
-//                elevator.ElevatorVoltageMotor2(1); // Voltage TBD
-//
-//            } else if (operatorGamepad.circle) {
-//                elevator.ElevatorVoltageMotor1(-1); // Voltage TBD
-//                elevator.ElevatorVoltageMotor2(-1); // Voltage TBD
-//            } else {
-//                elevator.ElevatorVoltageMotor1(0);
-//                elevator.ElevatorVoltageMotor2(0);
-//            }
-
-            /*
-            // Shooter
-                if (driverGamepad.right_bumper) {
-                    shooter.ShooterVoltage(1); // Voltage Pendant to Adjust
-                }   else {
-                    shooter.ShooterVoltage(0); // Voltage Pendant to Adjust
-                }*/
-
 
             // -- TELEMETRY -- //
 
