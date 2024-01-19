3e78493208218beedb7a7c1c4b6654e6485a1487
GabrielReynaFranco <a01572655@tec.mx>
2024-01-18 19:54:32 -0600

    Corrected unused variables and unused function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ElevatorMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ElevatorMove.java
@@ -8,12 +8,9 @@ import org.firstinspires.ftc.teamcode.Subsystems.Elevator;
 public class ElevatorMove extends CommandBase {
 
     private Elevator elevator;
-    private double voltage;
 
     //Este double va a ser parte del calculo para saber que tanto se tiene que extender
-    private double elevatorTimeToExtend;
-    private final double extendedHeight = 15; //Valor no final
-    private final double nonExtendedHeight = 0;
+
     private double elevatorHeight;
 
 
@@ -26,7 +23,6 @@ public class ElevatorMove extends CommandBase {
 
     @Override
     public void initialize() {
-        elevator.ElevatorHeight(elevatorHeight);
         elevator.ElevatorHeight(elevatorHeight);
     }
 
