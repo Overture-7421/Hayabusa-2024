4f02873abb54041c816dd088fc9ac96861d1c86a
Marco <A015726012@tec.mx>
2024-01-18 18:03:59 -0600

    Command Base ready to test and code review

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -9,7 +9,7 @@ public class Intake extends SubsystemBase {
     private DcMotor intake;
 
         public Intake(HardwareMap hardwareMap) {
-            intake = hardwareMap.get(DcMotor.class, "IntakeSubsystem");
+            intake = hardwareMap.get(DcMotor.class, "intake");
         }
 
         public void Voltage (double IntakeMotorSpeed){
