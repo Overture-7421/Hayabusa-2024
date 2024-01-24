2f727d1eebf4cebd9667b41effc8589a62e6ebee
GabrielReynaFranco <a01572655@tec.mx>
2024-01-24 13:19:28 -0600

    Change the name of the trajectories, did three basic autonomous and did a new band and intake command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommands/SpitPixels.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.AutonomousCommands;
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+
+import org.firstinspires.ftc.teamcode.Commands.MoveIntake;
+import org.firstinspires.ftc.teamcode.Commands.MoveBand;
+import org.firstinspires.ftc.teamcode.Subsystems.Band;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+
+public class SpitPixels extends ParallelCommandGroup {
+
+    public SpitPixels(Band band, Intake intake) {
+        addCommands(
+                new MoveBand(band, -1),
+                new MoveIntake(intake, -1));
+
+    }
+}
