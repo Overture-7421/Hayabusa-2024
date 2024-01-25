2aea089e02e80bec77de8a75453f77f6ee82de34
GabrielReynaFranco <a01572655@tec.mx>
2024-01-25 15:28:14 -0600

    Rigo's autonomous( red bottom) and Blue bottom completed with spit pixels command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousRedBottom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousRedBottom.java
@@ -41,17 +41,17 @@ Elevator elevator;
         elevator = new Elevator(hardwareMap);
 
         Trajectory redBottom = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(0,0,Rotation2d.fromDegrees(0)),
-                new Pose2d(2.6,0,Rotation2d.fromDegrees(90)),
-                new Pose2d(1.829,1.219,Rotation2d.fromDegrees(90)),
-                new Pose2d(1.7,1.219,Rotation2d.fromDegrees(0)))
+                        new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                        new Pose2d(1,0, Rotation2d.fromDegrees(0)),
+                        new Pose2d(1.6, -1.4, Rotation2d.fromDegrees(-90)),
+                        new Pose2d(0.8,-2.3, Rotation2d.fromDegrees(-180)))
                 , new TrajectoryConfig(1, 0.8));
 
 
         SequentialCommandGroup testCommandGroup = new SequentialCommandGroup(
                  new RamseteCommand(chassis, redBottom),
-                 new TurnToAngle(chassis,Rotation2d.fromDegrees(-90)),
-                 new SpitPixels(band, intake).withTimeout(2000)
+                new TurnToAngle(chassis,Rotation2d.fromDegrees(270)),
+                new SpitPixels(band, intake).withTimeout(4000)
                  //new Drop_pixels(elevator,arm,claw),
                  //new Drop_pixels(elevator,arm,claw)
         );
