02e1e1b2544e0b0c89a4ee48ef4de3cadb3e7441
AdrianZayas <a01572535@tec.mx>
2024-01-26 15:42:17 -0600

    Some adjustments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBlueBottom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBlueBottom.java
@@ -43,19 +43,14 @@ public class AutonomousBlueBottom extends LinearOpMode {
         Trajectory blueBottom = TrajectoryGenerator.generateTrajectory(Arrays.asList(
                         new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
                         new Pose2d(1,0, Rotation2d.fromDegrees(0)),
-                        new Pose2d(1.6, 1.4, Rotation2d.fromDegrees(90)),
-                        new Pose2d(0.8,2.3, Rotation2d.fromDegrees(180)))
-                , new TrajectoryConfig(1, 0.8));
-
-
-
+                        new Pose2d(1.6, 1.2, Rotation2d.fromDegrees(90)),
+                        new Pose2d(0.7,2, Rotation2d.fromDegrees(0)))
+                , new TrajectoryConfig(1, 0.5));
 
         SequentialCommandGroup testCommandGroup = new SequentialCommandGroup(
                  new RamseteCommand(chassis, blueBottom),
                  new TurnToAngle(chassis, Rotation2d.fromDegrees(90)),
                  new SpitPixels(band, intake).withTimeout(4000)
-                // new Drop_pixels(elevator,arm,claw),
-                //new Drop_pixels(elevator,arm,claw)
         );
 
         waitForStart();
