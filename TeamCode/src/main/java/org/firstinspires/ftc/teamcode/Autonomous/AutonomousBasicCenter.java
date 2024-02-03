0db64fca11b6775774a42f32618f8157e1695b30
Marco <A015726012@tec.mx>
2024-02-02 18:41:53 -0600

    Autonomous re-arranged distances

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBasicCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBasicCenter.java
@@ -45,7 +45,7 @@ public class AutonomousBasicCenter extends LinearOpMode {
 
         Trajectory basicCenter = TrajectoryGenerator.generateTrajectory(Arrays.asList(
                         new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
-                        new Pose2d(0.65, 0, Rotation2d.fromDegrees(0))),
+                        new Pose2d(0.7, 0, Rotation2d.fromDegrees(0))),
                         new TrajectoryConfig(1, 0.8));
 
         Trajectory returnTrajectory = TrajectoryGenerator.generateTrajectory(Arrays.asList(
